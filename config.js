af3cd5888d107b2e29aef0f8205676d426bac78a
pyChoke <86721025+pyChoke@users.noreply.github.com>
2021-06-30 20:26:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Trang ơi!',
+    introDesc: `Hôm nay Trang thế nào ? `,
+    btnIntro: 'touch it',
+    title: 'Hôm nay trời không trăng không sao 😙',
+    desc: 'Thế vì sao nay không on mic tâm sự tí nào ',
+    btnYes: 'Okela vào thôi <33',
+    btnNo: 'Không, nay tui bận :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Trang lại thích chơi với tui',
+    btnReply: 'Gửi cho tui',
+    reply: 'Vì py dễ thương',
+    mess: 'Tui biết mà 🥰',
+    messDesc: 'Tối nay 9h nhé ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
